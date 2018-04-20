222a6c9af1a1ea0c3d2dd88ef1f192a1573afba9
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2018-04-20 16:15:05 +0200

    added Expr class

---
+++ LoxInterpreter/AST/Expr.cs
@@ -0,0 +1,21 @@
+﻿namespace LoxInterpreter
+{
+    abstract class Expr
+    {
+
+    }
+
+    class BinaryExpr : Expr
+    {
+        public Expr Left { get; }
+        public Token Operator { get; }
+        public Expr Right { get; }
+
+        public BinaryExpr(Expr left, Token op, Expr right)
+        {
+            Left = left;
+            Operator = op;
+            Right = right;
+        }
+    }
+}
