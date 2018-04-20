1e299b01926a93cad0bf1c6d27f94e06d77415d0
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2018-04-20 12:29:59 +0200

    changed assembly name

--- LoxInterpreter/Program.cs
+++ LoxInterpreter/Program.cs
@@ -10,6 +10,27 @@ namespace LoxInterpreter
     {
         static void Main(string[] args)
         {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: cslox [script]");
+            }
+            else if (args.Length == 1)
+            {
+                RunFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+        }
+
+        private static void RunFile(string path)
+        {
+
+        }
+
+        private static void RunPrompt()
+        {
 
         }
     }
