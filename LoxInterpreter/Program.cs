1d26659accb985abba9a3b9e3d0800e0ce79c884
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2018-04-20 13:20:47 +0200

    added Scanner basic structure

--- LoxInterpreter/Program.cs
+++ LoxInterpreter/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace LoxInterpreter
 {
     class Program
     {
+        static bool hadError = false;
+
         static void Main(string[] args)
         {
             if (args.Length > 1)
@@ -26,12 +25,45 @@ namespace LoxInterpreter
 
         private static void RunFile(string path)
         {
+            string source = File.ReadAllText(path);
+            Run(source);
 
+            if (hadError)
+            {
+                Environment.Exit(65);
+            }
         }
 
         private static void RunPrompt()
         {
+            while (true)
+            {
+                Console.Write("> ");
+                Run(Console.ReadLine());
+                hadError = false;
+            }
+        }
 
+        private static void Run(string source)
+        {
+            var scanner = new Scanner(source);
+            var tokens = scanner.ScanTokens();
+
+            foreach (var t in tokens)
+            {
+                Console.WriteLine(t);
+            }
+        }
+
+        private static void Error(int line, string message)
+        {
+            Report(line, "", message);
+        }
+
+        private static void Report(int line, string where, string message)
+        {
+            Console.Error.WriteLine("[Line {0}] Error {1}: {2}", line, where, message);
+            hadError = true;
         }
     }
 }
---
+++ LoxInterpreter/Scanner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoxInterpreter
+{
+    class Scanner
+    {
+        public Scanner(string source)
+        {
+
+        }
+
+        public List<Token> ScanTokens()
+        {
+            return null;
+        }
+    }
+}
