2f80173bf55dfc2f78a8047cf49f1f6e3983e1ea
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2018-04-21 17:58:30 +0200

    added environment nesting

--- LoxInterpreter/Interpreter/Environment.cs
+++ LoxInterpreter/Interpreter/Environment.cs
@@ -4,10 +4,16 @@ namespace LoxInterpreter
 {
     class Environment
     {
+        private Environment enclosing;
         private Dictionary<string, object> values;
 
         public Environment()
+            : this(null)
+        { }
+
+        public Environment(Environment environment)
         {
+            enclosing = environment;
             values = new Dictionary<string, object>();
         }
 
@@ -24,6 +30,12 @@ namespace LoxInterpreter
                 return;
             }
 
+            if (enclosing != null)
+            {
+                enclosing.Assign(name, value);
+                return;
+            }
+
             throw new RuntimeError(name, "Undefined variable '" + name.Lexeme + "'.");
         }
 
@@ -32,6 +44,11 @@ namespace LoxInterpreter
             if (values.ContainsKey(name.Lexeme))
             {
                 return values[name.Lexeme];
+            }
+
+            if (enclosing != null)
+            {
+                return enclosing.Get(name);
             }
 
             throw new RuntimeError(name, "Undefined variable '" + name.Lexeme + "'.");
