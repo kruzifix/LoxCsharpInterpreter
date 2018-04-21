e401280fb326ca7c2340dddf63cc27a547bda6a9
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2018-04-21 13:43:52 +0200

    added basic interpreter implementation

--- LoxInterpreter/Interpreter/Interpreter.cs
+++ LoxInterpreter/Interpreter/Interpreter.cs
@@ -1,27 +1,95 @@
-﻿using System;
-
-namespace LoxInterpreter
+﻿namespace LoxInterpreter
 {
     class Interpreter : IVisitor<object>
     {
         public object VisitBinaryExpr(BinaryExpr expr)
         {
-            throw new NotImplementedException();
+            var left = Evaluate(expr.Left);
+            var right = Evaluate(expr.Right);
+
+            switch (expr.Operator.Type)
+            {
+                case TokenType.Greater:
+                    return (double)left > (double)right;
+                case TokenType.GreaterEqual:
+                    return (double)left >= (double)right;
+                case TokenType.Less:
+                    return (double)left < (double)right;
+                case TokenType.LessEqual:
+                    return (double)left <= (double)right;
+
+                case TokenType.BangEqual:
+                    return !IsEqual(left, right);
+                case TokenType.EqualEqual:
+                    return IsEqual(left, right);
+
+                case TokenType.Minus:
+                    return (double)left - (double)right;
+                case TokenType.Plus:
+                    if (left is double && right is double)
+                        return (double)left + (double)right;
+                    if (left is string && right is string)
+                        return (string)left + (string)right;
+                    break;
+                case TokenType.Slash:
+                    return (double)left / (double)right;
+                case TokenType.Star:
+                    return (double)left * (double)right;
+            }
+
+            // unreachable
+            return null;
         }
 
         public object VisitGroupingExpr(GroupingExpr expr)
         {
-            throw new NotImplementedException();
+            return Evaluate(expr.Expression);
         }
 
         public object VisitLiteralExpr(LiteralExpr expr)
         {
-            throw new NotImplementedException();
+            return expr.Value;
         }
 
         public object VisitUnaryExpr(UnaryExpr expr)
         {
-            throw new NotImplementedException();
+            var right = Evaluate(expr.Right);
+
+            switch (expr.Operator.Type)
+            {
+                case TokenType.Bang:
+                    return !IsTruthy(right);
+                case TokenType.Minus:
+                    return -(double)right;
+            }
+
+            // unreachable
+            return null;
+        }
+
+        private object Evaluate(Expr expr)
+        {
+            return expr.Accept(this);
+        }
+
+        private bool IsTruthy(object value)
+        {
+            if (value == null)
+                return false;
+            if (value is bool)
+                return (bool)value;
+            return true;
+        }
+
+        private bool IsEqual(object left, object right)
+        {
+            // nil is only equal to nil.
+            if (left == null && right == null)
+                return true;
+            if (left == null)
+                return false;
+
+            return left.Equals(right);
         }
     }
 }
