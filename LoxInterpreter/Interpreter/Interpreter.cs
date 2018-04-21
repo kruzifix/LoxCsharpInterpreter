c2c42942255cf584f73c2cf9369b1331b80db55f
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2018-04-21 13:05:29 +0200

    added Interpreter class

---
+++ LoxInterpreter/Interpreter/Interpreter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LoxInterpreter
+{
+    class Interpreter : IVisitor<object>
+    {
+        public object VisitBinaryExpr(BinaryExpr expr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitGroupingExpr(GroupingExpr expr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitLiteralExpr(LiteralExpr expr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitUnaryExpr(UnaryExpr expr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
