259582cc59ac441b7b4299561a9a327673337506
David Cukrowicz <dcukrowicz.mmt-b2016@fh-salzburg.ac.at>
2018-04-24 10:59:21 +0200

    added class instance

--- LoxInterpreter/Interpreter/LoxClass.cs
+++ LoxInterpreter/Interpreter/LoxClass.cs
@@ -1,12 +1,24 @@
-﻿namespace LoxInterpreter
+﻿using System.Collections.Generic;
+
+namespace LoxInterpreter
 {
-    class LoxClass
+    class LoxClass : ICallable
     {
         public string Name { get; private set; }
 
         public LoxClass(string name)
         {
             Name = name;
+        }
+        
+        public int Arity()
+        {
+            return 0;
+        }
+
+        public object Call(Interpreter interpreter, List<object> arguments)
+        {
+            return new LoxInstance(this);
         }
 
         public override string ToString()
---
+++ LoxInterpreter/Interpreter/LoxInstance.cs
@@ -0,0 +1,17 @@
+﻿namespace LoxInterpreter
+{
+    class LoxInstance
+    {
+        private LoxClass klass;
+
+        public LoxInstance(LoxClass klass)
+        {
+            this.klass = klass;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("<instance {0}>", klass.Name);
+        }
+    }
+}
